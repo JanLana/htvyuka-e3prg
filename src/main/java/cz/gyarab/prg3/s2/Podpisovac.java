b7c22a3c05c44c08dfae9ed6e870d73dcd296cfa
Jan Lana <jan.lana@gyarab.cz>
2022-10-18 11:22:25 +0200

    hodina 17.10.

--- src/main/java/cz/gyarab/prg3/s2/Podpisovac.java
+++ src/main/java/cz/gyarab/prg3/s2/Podpisovac.java
@@ -1,22 +1,32 @@
 package cz.gyarab.prg3.s2;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
-import java.security.SignatureException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Podpisovac {
-    public static void main(String[] args) throws NoSuchAlgorithmException, SignatureException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException, IOException {
         String msg = "Pepa z Depa dluzi Janu Lanovi 1milion Kc.";
 
-       // Signature signGenerator = Signature.getInstance("SHA1withRSA");
-       // signGenerator.initSign(privateKey);
-       // signGenerator.update(msg.getBytes());
+        byte[] priv = Files.readAllBytes(Paths.get("mujTajny.key"));
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(priv));
 
-       // byte[] sign = signGenerator.sign();
+        Signature signGenerator = Signature.getInstance("SHA1withRSA");
+        signGenerator.initSign(privateKey);
+        signGenerator.update(msg.getBytes());
 
-       // System.out.println(msg);
-       // Arrays.toString(sign);
+        byte[] sign = signGenerator.sign();
+
+        System.out.println(msg);
+        System.out.println("====");
+        System.out.println(Arrays.toString(sign));
+        System.out.println(Base64.getEncoder().encodeToString(sign));
     }
 }
