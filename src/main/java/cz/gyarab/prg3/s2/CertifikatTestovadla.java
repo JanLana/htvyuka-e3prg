8cfcb0d54163b35e580918b0cd9271392fa2d141
Jan Lana <jan.lana@gyarab.cz>
2022-11-03 16:47:08 +0100

    hodina 21.10.

---
+++ src/main/java/cz/gyarab/prg3/s2/CertifikatTestovadla.java
@@ -0,0 +1,22 @@
+package cz.gyarab.prg3.s2;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import java.io.IOException;
+import java.net.URL;
+import java.security.cert.Certificate;
+
+public class CertifikatTestovadla {
+    public static void main(String[] args) throws IOException {
+        URL destinationURL = new URL("https://gyarab.ddns.net");
+        HttpsURLConnection conn = (HttpsURLConnection) destinationURL.openConnection();
+        conn.connect();
+        Certificate[] certs = conn.getServerCertificates();
+
+        for (Certificate c: certs) {
+            System.out.println("===================================");
+            System.out.println(c);
+
+        }
+    }
+}
